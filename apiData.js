d23575ec9c54fdb85e433306296e54c2e72e8178
rahulnagarkoti <nagarkotirahul@hotmail.com>
2020-12-11 01:27:21 +1100

    api request handling implemented

--- apiData.js
+++ apiData.js
@@ -1,66 +1,62 @@
-const request = require('request');
-var blogData="";
+const fetch = require('node-fetch');
+
 module.exports = 
 {
-    allData: function()
+    allData: async function()
     {
+        var data;
         let url='https://www.healthcare.gov/api/blog.json';
-    //api request
-    request(url,{json:true},(err,res,body)=>
-    {
-        if(err)
-        {
-            console.log("error");
-            console.log(err);
-        }
-         blogData=body.blog;
+        const apiResponse = await fetch(url);
+        const json = await apiResponse.json()
+        console.log(json.blog);
+        return json.blog;
 
-         blogData.forEach((item)=>
-         {
-             console.log(item.lang);
-
-         });
-    });
-
-    
     },
     filterData: function(lang,topic,date)
     {
+        resarray=[];
         //filter data
+        console.log(this.allData());
+        this.allData().forEach(element => {
+            if(element.lang.Contains(lang))
+            {
+                resarray.add(element);
+
+            } 
+        });
+
     },
-    languagesList: function()
+    languagesList: function(data)
     {
-        return [{id: "en", lang: "English"},{id: "esp",lang:"Spanish"},{id: "deu", lang:"German"}];
+        var languageArray=[];
+        console.log(data);
+        data.forEach(item=>
+            {
+                if(item.lang == "en")
+                {
+                    languageArray.push({id: "en", lang: "English"});
+                }
+                else if(item.lang =="esp")
+                {
+                    languageArray.push({id: "esp", lang: "Spanish"});
+                }                
+                else
+                {
+                    languageArray.push({id: item.lang, lang: "Other"});
+                }
+            });
+            
+
+        return languageArray;
     },
-    posts: function()
+    posts: function(data)
     {
-            // fake posts to simulate a database
-    const posts = [
-        {
-          id: 1,
-          author: 'John',
-          title: 'Templating with EJS',
-          body: 'Blog post number 1'
-        },
-        {
-          id: 2,
-          author: 'Drake',
-          title: 'Express: Starting from the Bottom',
-          body: 'Blog post number 2'
-        },
-        {
-          id: 3,
-          author: 'Emma',
-          title: 'Streams',
-          body: 'Blog post number 3'
-        },
-        {
-          id: 4,
-          author: 'Cody',
-          title: 'Events',
-          body: 'Blog post number 4'
-        }
-      ];
+        var posts=[];
+        console.log(data);
+        data.forEach(item=>
+            { 
+                posts.push({title: item.title, content:item.content, date:item.date})                
+            });
         return posts; 
     }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,38 @@
 const express = require('express');
 const router = express.Router();   
-const apiData = require("../apiData")
+const apiData = require("../apiData");
+const fetch = require('node-fetch');
+
 
 //requests to /
-router.get('/',function(req,res){
-    console.log(apiData.allData());
-    res.render('index',{languages:  apiData.languagesList(), posts: apiData.posts()});
+router.get('/', async function(req,res){
+  let url='https://www.healthcare.gov/api/blog.json';
+  const apiResponse = await fetch(url);
+  const json = await apiResponse.json()
+  const data= json.blog;
+  var languageList=apiData.languagesList(data);
+  var postsList=apiData.posts(data);
+ 
+
+  res.render('index',{languages:  languageList, posts: postsList});
+
+  //  apiData.allData().then((data)=>
+  //   {
+  //     console.log(data);
+  //     //languageList= apiData.languagesList(data);
+
+  //     res.render('index',{languages:  [{id: 1, lang: "English"},{id: 2,lang:"Spanish"},{id: 3, lang:"German"}], posts: apiData.posts()});
+  //   });
   });
   
+
   //requests to /getData
 router.post('/getData',function(req,res,next){
     var lang= req.body.language;
     var topic= req.body.topic;
     var date= req.body.date;
     console.log(lang,topic,date)
-    res.render('index',{languages: apiData.languagesList(),posts: apiData.posts()});
+    res.render('index',{languages: languages,posts: posts});
 });
 
 module.exports = router;
